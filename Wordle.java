a42cb14607534b68a3ef86f21b757b12ddc4d22e
Josh Houlding <jdh102040@gmail.com>
2022-03-06 18:21:02 -0800

    Updated program to help with an entire Wordle game
    The program now assists with an entire Wordle game involving several guesses, and not just a single guess.
    
    KNOWN ISSUE: The program still suggests some words that contain letters known to not be in the word. This doesn't impede functionality too much, but I'll still be looking into how to correct it.

--- Wordle.java
+++ Wordle.java
@@ -1,28 +1,55 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Wordle {
+
+    static ArrayList<String> words = new ArrayList<String>();
+
     public static void main(String[] args) throws FileNotFoundException {
         // Intro
         System.out.println();
         System.out.println(
                 "Welcome! This tool will help you out in Wordle by taking your last guess along with information about what color each letter was, and giving you some suggestions!");
-        System.out.println(
-                "Enter your last guess below, and then the color of each letter. Enter x for gray, y for yellow, and g for green. EXAMPLE: tears yxxyx");
 
+        // Read the file into an arraylist for processing
         File f = new File("5letterwords.txt");
-
-        // Manage the user input
-        Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
-        String[] userInputArray = userInput.split(" ");
+        Scanner sc = new Scanner(f);
+        while (sc.hasNextLine()) {
+            words.add(sc.nextLine());
+        }
+        sc.close();
 
         // The moment of truth. Find words in the text file matching the criteria
         // outlined by the previous guess.
-        System.out.println();
-        System.out.println("Here are some guesses you could make.");
-        findWords(f, userInputArray[0], userInputArray[1]);
+        Scanner sc2 = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+
+            System.out.println();
+            System.out.println(
+                    "Enter your last guess below, and then the color of each letter. Enter x for gray, y for yellow, and g for green. EXAMPLE: tears yxxyx");
+            System.out.println("If you got it, enter \"quit\" to quit. ");
+
+            // Manage the user input
+            String userInput = sc2.nextLine();
+            // If the user gets the word, allow them to quit
+            if (userInput.toLowerCase().equals("quit")) {
+                System.out.println("Congratulations!");
+                break;
+            }
+            String[] userInputArray = userInput.split(" ");
+            System.out.println();
+            System.out.println("Here are some guesses you could make.");
+            findWords(words, userInputArray[0], userInputArray[1]);
+
+            // Print out and format the list contents
+            for (String s : words) {
+                System.out.print(s + " | ");
+            }
+            System.out.println();
+        }
+        sc2.close();
     }
 
     // Checks if a word contains a letter in a certain position
@@ -62,34 +89,35 @@ public class Wordle {
     // tears yxxyx means "find all words containing t but not in that position, not
     // containing e and a, containing r but not in that position, and not containing
     // s."
-    public static void findWords(File f, String word, String colorSequence) throws FileNotFoundException {
-        Scanner sc = new Scanner(f);
+    public static void findWords(ArrayList<String> s, String word, String colorSequence) throws FileNotFoundException {
         boolean[] conditions = new boolean[5];
 
-        while (sc.hasNextLine()) {
-            String currentWord = sc.nextLine();
+        for (int i = 0; i < s.size(); i++) {
+            String currentWord = s.get(i);
             // Populate the conditions array with conditions from the color sequence
-            for (int i = 0; i < 5; i++) {
-                conditions[i] = checkCondition(currentWord, word, colorSequence.charAt(i), i);
+            for (int j = 0; j < 5; j++) {
+                conditions[j] = checkCondition(currentWord, word, colorSequence.charAt(j), j);
             }
 
-            // Check the boolean array to check if all conditions are true for the word
-            // being analyzed. If yes, then print it out.
-            if (!checkBooleanArray(conditions)) {
-                System.out.print(currentWord + " | ");
+            // Check the boolean array to check if any conditions for the current word are
+            // false. If so, remove the word from consideration.
+            if (checkBooleanArray(conditions)) {
+                s.remove(i);
+                i--;
             }
         }
-        sc.close();
     }
 
     // Apply the color condition from the first word to the second
     public static boolean checkCondition(String currentWord, String word, char color, int index) {
         if (color == 'x') {
-            //Conditions are for repeated letters
-            //If the word really does not contain a letter, proceed with that information normally.
-            if(grayLetter(currentWord, word.charAt(index))) {
-            return (grayLetter(currentWord, word.charAt(index)));
-            //If the word does contain a letter but at least one of them is gray, look for words that don't have that letter in that position. 
+            // Conditions are for repeated letters
+            // If the word really does not contain a letter, proceed with that information
+            // normally.
+            if (grayLetter(currentWord, word.charAt(index))) {
+                return (grayLetter(currentWord, word.charAt(index)));
+                // If the word does contain a letter but at least one of them is gray, look for
+                // words that don't have that letter in that position.
             } else {
                 return (!greenLetter(currentWord, word.charAt(index), index));
             }
@@ -111,14 +139,16 @@ public class Wordle {
         return false;
     }
 
-    //Check if a certain letter has occurred before in the word (sometimes a guess will have a repeated letter, the first being yellow or green and the second being gray)
+    // Check if a certain letter has occurred before in the word (sometimes a guess
+    // will have a repeated letter, the first being yellow or green and the second
+    // being gray)
     public static boolean letterOccurredAlready(String word, char letter, int secondIndex) {
-        for(int i = 0; i < secondIndex; i++) {
-            if(word.charAt(i) == letter) {
-                return true; 
+        for (int i = 0; i < secondIndex; i++) {
+            if (word.charAt(i) == letter) {
+                return true;
             }
         }
-        return false; 
+        return false;
     }
 
 }
